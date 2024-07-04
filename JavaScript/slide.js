f801526122f7776fb96fa082968f4b4617a7725f
Guttenbergh <106270381+Guttenbergh@users.noreply.github.com>
2024-07-04 17:23:46 -0300

    Subi os arquivos do meu site

---
+++ JavaScript/slide.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var radio = document.querySelectorAll('.manual-btn');
+    var cont = 1;
+
+    document.getElementById('radio1').checked = true;
+
+    setInterval(() => {
+        proximaImg();
+    }, 5000);
+
+    function proximaImg() {
+        cont++;
+
+        if (cont > 3) {
+            cont = 1;
+        }
+
+        document.getElementById('radio' + cont).checked = true;
+    }
+});
